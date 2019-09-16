c5d27bfc8f097e0bc08b80b7ea9bc168b08fb739
markusjsommer <markusjsommer@gmail.com>
2019-09-15 20:02:55 -0400

    very bad workaround to get results, TODO figure out why some genomes make it stall without error

--- c++/main.cpp
+++ c++/main.cpp
@@ -15,12 +15,16 @@ int main() {
 //    std::string filepath_in = "/ccb/salz4-4/markus/markraken/data/databases/miniSeq+H/DB_small.fa";
 //    std::string DB_path = "/ccb/salz4-4/markus/markraken/data/refseq_singlestrain/refseq.fa";
     std::string DB_HPC_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_HPC.fa";
-    std::string index_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_index.markmap";
-    std::string marker_path = "/ccb/salz4-4/markus/markraken/data/compressed/mn1000.marker";
-    std::string markerized_seq_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_HPC_ml10_mn_1000.markerized";
-    std::string taxid_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_HPC_ml10_mn_1000.taxid";
 
+//    std::string index_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_index.markmap";
+//    std::string marker_path = "/ccb/salz4-4/markus/markraken/data/compressed/mn1000.marker";
+//    std::string markerized_seq_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_HPC_ml10_mn_1000.markerized";
+//    std::string taxid_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_HPC_ml10_mn_1000.taxid";
 
+    std::string index_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_index_mn8000.markmap";
+    std::string marker_path = "/ccb/salz4-4/markus/markraken/data/compressed/mn8000.marker";
+    std::string markerized_seq_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_HPC_ml10_mn_8000.markerized";
+    std::string taxid_path = "/ccb/salz4-4/markus/markraken/data/compressed/refseq_HPC_ml10_mn_8000.taxid";
 
 //    // homopolymer compress fasta and save as new fasta
 //    HPC compressor;
@@ -38,16 +42,16 @@ int main() {
 
 
     // marker parameters
-    uint32_t n_markers = 1000;
+    uint32_t n_markers = 8000;
     uint32_t marker_length = 10;
     uint32_t seed = 2019;
 
 
     // generate markers and markerize HPC sequence
     markerizer M;
-//    M.generate_markers(n_markers,marker_length, seed);
-//    M.save_markers(marker_path);
-    M.load_markers(marker_path);
+    M.generate_markers(n_markers,marker_length, seed);
+    M.save_markers(marker_path);
+//    M.load_markers(marker_path);
 
 
     // extract all markers from the compressed sequence
@@ -73,6 +77,7 @@ int main() {
     oarchive2(marker_seqs);
     f_out2.close();
 
+
 //    // load markerized sequence and taxids
 //    std::vector<std::vector<uint32_t>> marker_seqs;
 //
@@ -86,15 +91,15 @@ int main() {
 //    cereal::BinaryInputArchive iarchive2(f_in2);
 //    iarchive2(taxids);
 //    f_in2.close();
-
-//    for (int i=0; i<marker_seqs.size(); i++){
+//
+//    for (int i=0; i<10; i++){
 //        for (int j=0; j<marker_seqs[i].size();j++){
 //            std::cout << marker_seqs[i][j] << std::endl;
 //        }
 //    }
 
 
-    // load taxonomy for LCA calculation
+//    // load taxonomy for LCA calculation
 //    std::string names_path = "/ccb/salz4-4/markus/markraken/data/ncbi_taxonomy/names.dmp";
 //    std::string nodes_path = "/ccb/salz4-4/markus/markraken/data/ncbi_taxonomy/nodes.dmp";
 //    std::string merged_path = "/ccb/salz4-4/markus/markraken/data/ncbi_taxonomy/merged.dmp";
@@ -108,15 +113,39 @@ int main() {
 //    marker_index.read_taxonomy(names_path, nodes_path, merged_path);
 //
 //    for (int i=0; i<marker_seqs.size(); i++){
-//        std::vector<uint32_t> &singleseq = marker_seqs[i];
-//        TaxID id = taxids[i];
-//        for (int j=0; j<singleseq.size()-marK; j++){
-//            std::vector<uint32_t> marker(&singleseq[j], &singleseq[j+marK]); // TODO do this without copying data
-//            marker_index.add_pair(marker, TaxID(id));
+//        std::vector<int> FOOSKIP; // TODO figure out why some make it stall, this is a temporary solution
+//        FOOSKIP.push_back(1763);
+//        FOOSKIP.push_back(2054);
+//        FOOSKIP.push_back(2857);
+//        FOOSKIP.push_back(2858);
+//        FOOSKIP.push_back(2859);
+//        FOOSKIP.push_back(2867);
+//        FOOSKIP.push_back(3018);
+//        FOOSKIP.push_back(3380);
+//        FOOSKIP.push_back(4607);
+//        FOOSKIP.push_back(5029);
+//        FOOSKIP.push_back(5040);
+//        FOOSKIP.push_back(5858);
+//
+//        if (std::find(FOOSKIP.begin(), FOOSKIP.end(), i) == FOOSKIP.end()) {
+//
+//            std::cout << taxids[i] << std::endl;
+//
+//            if (i % 100 == 0) {
+//                std::cout << i << " of " << marker_seqs.size() << std::endl;
+//            }
+//            std::cout << i << " of " << marker_seqs.size() << std::endl;
+//
+//            std::vector<uint32_t> &singleseq = marker_seqs[i];
+//            TaxID id = taxids[i];
+//            for (int j = 0; j < singleseq.size() - marK; j++) {
+//                std::vector<uint32_t> marker(&singleseq[j], &singleseq[j + marK]); // TODO do this without copying data
+//                marker_index.add_pair(marker, TaxID(id));
+//            }
 //        }
 //    }
-//
 //    marker_index.save_index(index_path);
+
 
 //    // load index
 //    // TODO index requires loading a taxonomy to not crash, probably due to way it is started, fix this
@@ -128,7 +157,7 @@ int main() {
 //    marker_index.read_taxonomy(names_path, nodes_path, merged_path);
 //    marker_index.load_index(index_path);
 //
-//    int idx = 5;
+//    int idx = 112;
 //    for (int i=idx; i<idx+1; i++){
 //        for (int j=0; j<marker_seqs[i].size()-marK; j++){
 //            std::vector<uint32_t> testmarKmer(&marker_seqs[i][j], &marker_seqs[i][j+marK]);
